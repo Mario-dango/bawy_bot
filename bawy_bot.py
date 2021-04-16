4913e193589d20541116eb6c1c82a9b927ddb5b1
Mario-dango <mario.dango@users.noreply.github.com>
2021-04-16 14:25:26 -0300

    prueba enviar sticker

--- bawy_bot.py
+++ bawy_bot.py
@@ -7,22 +7,33 @@ import telebot
 
 API_TOKEN = '1733267643:AAG0KXIZdVUMGoI-g1Y1bg5osjrFhnGZKKQ'
 
-bot = telebot.TeleBot(API_TOKEN)
+bawy = telebot.TeleBot(API_TOKEN)
+
 
 
 # Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
+@bawy.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
+    bawy.reply_to(message, """\
 Hola a todos yo soy BawyBot... etoou... también puedes llamarme BB hehe.
 Pues.. mi amo me ha creado para ser un asistente domótico y estaré en cosntante modificación, espero dar lo mejor de mí n.n\
 """)
+    sti = open('/files/stickers/hello.webp', 'rb')
+    bawy.send_sticker(message, sti)
+    bawy.send_sticker(message, "FILEID")
 
+#def sendSticker(message):|
+#    # sendSticker
+#    sti = open('/files/stickers/aww.webp', 'rb')
+#    tb.send_sticker(chat_id, sti)
+#    tb.send_sticker(chat_id, "FILEID")
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
+@bawy.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.reply_to(message, message.text)
+
+    bawy.reply_to(message, message.text)
 
 
-bot.polling()
+
+bawy.polling()
